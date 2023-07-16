5aafc4056e507e73ad4a109205253fed0848d509
Gal Palas <galpalas89@gmail.com>
2023-07-16 12:57:02 +0300

    Refactor entity interfaces

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,6 @@
-import { Genre } from "./Genre";
-import { Platform } from "./Platform";
-import { Publisher } from "./Publisher";
+import { Genre, Platform, Publisher } from "./index";
 
-export interface Game {
+export default interface Game {
   id: number;
   slug: string;
   name: string;
